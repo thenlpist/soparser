370ca1e9c74ba28662cdd0560d7e6de8766b7611
thenlpist <abobicesaevior@gmail.com>
2025-06-23 13:06:36 -0400

    version bump to 0.1.3
    Please enter the commit message for your changes. Lines starting

--- rtk/openai_parser.py
+++ rtk/openai_parser.py
@@ -1,3 +1,4 @@
+import importlib.metadata
 import random
 from typing import Optional
 import logging
@@ -10,10 +11,10 @@ from rtk.resume_dataclass import Resume, ResumeSerializer
 from rtk.validation import Validation
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 # create file handler that logs debug and higher level messages
 sh = logging.StreamHandler()
-sh.setLevel(logging.INFO)
+sh.setLevel(logging.DEBUG)
 formatter = logging.Formatter("%(asctime)s: %(levelname)s: %(message)s")
 sh.setFormatter(formatter)
 logger.addHandler(sh)
@@ -25,15 +26,31 @@ class OAiParser:
 
     def __init__(self, openai_key, config: Optional[dict]):
         self.config = config
+        flags = {(f["name"]): {"enabled": f["enabled"], "environment": f.get("environment", None) } for f in config["flags"]}
+        experiment = flags.get("experiment", {})
+        current_env = config.get("current_env", "")
+        if not experiment:
+            self.test = False
+        else:
+            enabled = experiment.get("enabled", False)
+            environment = experiment.get("environment", None)
+            if enabled and environment == current_env:
+                self.test = True
+            else:
+                self.test = False
+
+        logger.info(f"(OAiParser) config.test: {self.test}")
         self.validate = Validation()
         self.model = "gpt-4o-2024-08-06"
         openai_key = openai_key if openai_key else os.environ.get("OPENAI_API_KEY", None)
         self.openai_is_available = True
         if not openai_key:
-            logger.error("API_KEY_NOT_DEFINED: OpenAI API key is not defined as an environment variable")
+            logger.error("(OAiParser) OpenAI API key is not defined as an environment variable")
             self.openai_is_available = False
         self.client = OpenAI(api_key=openai_key)
         self.serializer = ResumeSerializer()
+        self.var = ""
+        self.version_string = importlib.metadata.version("rtk")
 
     def parse(self, text):
         valid_key = self._validate_key()
@@ -43,11 +60,11 @@ class OAiParser:
                     "is_valid_jsonresume": False,
                     "jsonresume": {}}
         head = text[:100].replace("\n", " ")
-        logger.info(f"Calling OpenAI parser for text: {head}...")
+        logger.info(f"(OAiParser) Calling OpenAI parser for text: {head}...")
         resume, prompt_tokens, completion_tokens, generation_time = self._query_openai(text)
         num_tokens = prompt_tokens + completion_tokens
         num_chars = len(text)
-        logger.debug("Validating returned object...")
+        # logger.debug("Validating returned object...")
         valid_json, valid_json_resume = self.validate.validate_json_w_pydantic(resume)
         try:
             name = resume["basics"]["name"]
@@ -63,35 +80,48 @@ class OAiParser:
             "generation_time": generation_time,
             "num_chars": num_chars,
             "num_tokens": num_tokens,
-            "jsonresume": resume
+            "jsonresume": resume,
+            "sop": f"{self.version_string}{self.var}"
         }
 
     def parse_standalone(self, text):
-        text = self._perturb_text(text)
+        text, var = self._perturb_text(text)
         response = self.parse(text)
         response, statuscode = self.validate.compute_statuscode(response)
         response["statuscode"] = statuscode
+        response["var"] = var
         return response
 
     def _perturb_text(self, text):
-        if self.config.get("test", False):
-            if random.random() < 0.5:
-                logger.debug("Perturb 1")
-                text = text[:3000]
+        var = ""
+        if self.test:
+            time.sleep(3)
+            if random.random() < 0.3:
+                offsets = [2000, 3000, 4000]
+                offset = random.choice(offsets)
+                var = f"p1-{offset}"
+                text = text[:offset]
             elif random.random() < 0.3:
-                logger.debug("Perturb 2")
-                text = text.replace("\n", " ")
-            elif random.random() < 0.3:
-                logger.debug("Perturb 3")
-                items = text.split("\n")
-                num_items = len(items)
-                third = int(num_items / 3) * 2
-                start = items[:third]
-                end = items[third:]
+                var = "p2"
+                lines = text.split("\n")
+                num_lines = len(lines)
+                offset = min(20, int(num_lines / 4))
+                var = f"p2-{offset}"
+                start = lines[:offset]
+                end = lines[offset:]
                 random.shuffle(end)
                 new_items = start + end
-                return "\n".join(new_items)
-        return text
+                text = "\n".join(new_items)
+            elif random.random() < 0.3:
+                var = "p3"
+                text = text[300:].replace("\n", " ")
+            else:
+                var = "n"
+        logger.debug(f"(OAiParser) perturbation: {var}")
+        suffix = f".{var}" if var else ""
+        self.var = f".{var}" if var else ""
+        return text, var
+
 
     def _validate_key(self):
         if self.openai_is_available == False:
@@ -103,10 +133,10 @@ class OAiParser:
         t1 = time.time()
         try:
             resume_obj, prompt_tokens, completion_tokens = self._get_completion(text)
-            logger.info("Serializing response...")
+            logger.debug("(OAiParser) Serializing response...")
             resume = self.serializer.to_json_resume(resume_obj)
         except Exception as e:
-            logger.error(f"Error encountered while parsing OpenAI response: {e}")
+            logger.error(f"(OAiParser) Error encountered while parsing OpenAI response: {e}")
             resume = {}
             prompt_tokens = 0
             completion_tokens = 0
@@ -128,8 +158,8 @@ class OAiParser:
             resume_obj = completion.choices[0].message.parsed
             prompt_tokens = completion.usage.prompt_tokens
             completion_tokens = completion.usage.completion_tokens
-            logger.info(f"OpenAI-Usage => prompt_tokens: {prompt_tokens}  completion_tokens: {completion_tokens}")
+            logger.info(f"(OAiParser) OpenAI-Usage => prompt_tokens: {prompt_tokens}  completion_tokens: {completion_tokens}")
             return resume_obj, prompt_tokens, completion_tokens
         except Exception as e:
-            logger.error(f"StructuredOutputs Exception: {e}")
+            logger.error(f"(OAiParser) StructuredOutputs Exception: {e}")
             return None, 0, 0
